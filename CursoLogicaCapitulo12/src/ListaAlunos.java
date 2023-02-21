10f3ff92c3e9bc4793b16aacebac6ff226f95433
Jean Cunha <jecunha@MacBook-Pro-de-Usuario.local>
2023-02-20 23:03:33 -0300

    Adicionando e removendo itens na lista

---
+++ CursoLogicaCapitulo12/src/ListaAlunos.java
@@ -0,0 +1,44 @@
+
+public class ListaAlunos {
+	static final int QUANTIDADE_LISTA = 5;
+    Aluno[] lista = new Aluno[QUANTIDADE_LISTA];
+    int tamanhoLista = 0;
+    void adicionar(Aluno aluno){
+        if(lista.length == tamanhoLista){
+            Aluno[] novaLista = new Aluno[QUANTIDADE_LISTA + lista.length];
+            for(int i = 0; i < lista.length; i++){
+                novaLista[i] = lista[i];
+            }
+            lista = novaLista;
+        }
+        lista[tamanhoLista] = aluno;
+        tamanhoLista++;
+    }
+    Aluno obter(int indice){
+        return lista[indice];
+    }
+    int tamanho(){
+        return tamanhoLista;
+    }
+    void remover(Aluno aluno){
+        for(int i = 0; i < tamanhoLista; i++){
+            Aluno a = lista[i];
+            if(a != null && a.equals(aluno)){
+                //remover o aluno do array
+                remover(i);
+                break;
+            } else if(a == null && aluno == null){
+                remover(i);
+                break;
+            }
+        }
+    }
+    void remover(int indice){
+        int indiceInicial = indice + 1;
+        for(int i = indiceInicial; i < tamanhoLista; i++){
+            lista[i - 1] = lista[i];
+        }
+        tamanhoLista--;
+        lista[tamanhoLista] = null;
+    }
+}
