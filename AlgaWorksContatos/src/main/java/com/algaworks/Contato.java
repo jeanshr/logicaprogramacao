de7dd9b5316aa2fbb9683e4db9240f15aca51747
Jean Cunha <jecunha@MacBook-Pro-de-Usuario.local>
2023-02-22 21:49:31 -0300

    Projeto final: adicionando contatos a lista

---
+++ AlgaWorksContatos/src/main/java/com/algaworks/Contato.java
@@ -0,0 +1,37 @@
+package com.algaworks;
+
+public class Contato {
+	private String nome;
+	private String telefone;
+	private String id;
+	
+	//construtor padrão da classe
+	public Contato() {} 
+	
+	//construtor para popular a lista de contatos
+	public Contato(String id, String nome, String telefone) {
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
